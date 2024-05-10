2cd824a970483d684267cb77f05e8f84191bc1c7
Mariela Rocha <marirocodev@gmail.com>
2024-05-10 02:09:31 -0600

    Emoji button works now!
    There is a small emoji button that changes when you click on it :D

--- project_manager/src/assets/svg/emojis/emojisDirectory.jsx
+++ project_manager/src/assets/svg/emojis/emojisDirectory.jsx
@@ -1,4 +1,4 @@
-import Happy from './happy.svg?react';
+import Happy from './happy.svg';
 import Relaxed from './relaxed.svg';
 import Sad from './sad.svg';
 import Winking from './winking.svg';
--- project_manager/src/components/buttons/EmojiButton.jsx
+++ project_manager/src/components/buttons/EmojiButton.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {Happy,Relaxed,Sad,Winking} from'../../assets/svg/emojis/emojisDirectory.jsx';
+import './EmojiButton.css';
 
 export default function EmojiBtn(){
     const emojiArr = [Happy,Relaxed,Sad,Winking];
     const [currentEmoji, setEmoji] = useState(0);
-    function handleClick(){
-        //setEmoji(() => );
-        console.log('Yay!');
+     function handleClick(){
+
+        setEmoji((prevEmoji) => { 
+            if(prevEmoji >= emojiArr.length-1){
+                return 0;
+            }else{
+                return prevEmoji+1;
+            }
+            
+        });
     }
 
     return(
-            <button onClick={handleClick}>
-                {emojiArr[currentEmoji]}
-            </button>
+        <>
+            <div style={{mask: `url(${emojiArr[currentEmoji]}) 100% 100% / contain no-repeat`}} className="emoji" onClick={handleClick}/>
+        </>
     );  
 }
