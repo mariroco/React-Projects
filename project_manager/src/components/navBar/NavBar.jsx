44a7296c31bb5b02858bfb8331557ee0ab5222d2
Mariela Rocha <marirocodev@gmail.com>
2024-05-10 02:38:42 -0600

    NavBar title

--- project_manager/src/components/navBar/NavBar.jsx
+++ project_manager/src/components/navBar/NavBar.jsx
@@ -1,4 +1,15 @@
 import EmojiBtn from "../buttons/EmojiButton";
 export default function NavBar(){
-    return(<><EmojiBtn/></>);
+    return(
+    <div style={{display: 'flex', flexDirection: 'row', width:' fit-content', height: 'fit-content', margin: '1rem'}}>
+        <EmojiBtn/>
+        <div>
+            <h1>
+                My
+                <br/>
+                Project Manager
+            </h1>
+        </div>
+        
+    </div>);
 }
