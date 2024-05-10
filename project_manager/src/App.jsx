0b0483858c7ef540644b7010fed1eb98a623ee06
Mariela Rocha <marirocodev@gmail.com>
2024-05-09 21:57:14 -0600

    Project Manager base project
    Just a basic css grid structure on React

---
+++ project_manager/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css' 
+
+function App() {
+
+  return (
+  <div className='container'>
+    <nav>Nav</nav>
+    <div id='sideBar'>sideBar</div>
+    <div id='contentArea'>Content here</div>
+  </div>
+  );
+}
+
+export default App
